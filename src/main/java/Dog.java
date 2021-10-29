ba5afe16cd8c93a8c6b89c6f2b7108db00f8c1ec
natewong <nwwong17@gmail.com>
2021-10-29 11:52:34 -0700

    Created a Dog class inherited from a Pet class for APCS HW

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,27 @@
 public class Dog extends Pet {
-    // Instance variable(s)
+    private String breed;
+    public Dog(String name, int age, String breed) { //dog constructor
+        super(name, age);
+        this.breed=breed;
+    }
+    public Dog() {//generic dog constructor
+        super("Max", 1);
+        this.breed="Golden retriever";
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println("Woof!");
+    }
+    @Override
+    public String toString() {
+        return "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + this.breed;
+    }
 
+    public String getBreed() {
+        return this.breed;
+    }
 
-    // Constructors
-    
-
-    // makeNoise() method
-
-
-    // toString method
-
-
-    // Getter
-
-
-    // Setter
+    public void setBreed(String str) {
+        this.breed=str;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,13 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog genericDog = new Dog();
+        Dog rover = new Dog("Rover", 5, "Corgi");
+        System.out.println(genericDog);
+        System.out.println(rover);
+        System.out.println("Noise:");
+        rover.makeNoise();
+        System.out.println(rover.getBreed());
+        System.out.println(rover.getAge());
+        System.out.println(rover.getName());
     }
 }
